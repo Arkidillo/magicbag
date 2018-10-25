082a30af94bcd7d5955d1216ca53c9d97c5c4052
Devin Kopp <devinkopp@gwu.edu>
2018-10-24 23:53:11 -0400

    Add DB

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request, session, flash, Markup
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+import os
+
+
 app = Flask(__name__)
+#app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///magicbag-form"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://zpxaecnydqxxhj:258722efa1e462c8c825f0e63f575b8d1c0c9e8535fdfda058b6b28ba05e01bf@ec2-107-20-211-10.compute-1.amazonaws.com:5432/d1p1mua37cs8ho"
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
