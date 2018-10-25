185e819d7f6fc398ed5043e8ed3b4e1e3004489b
MrGarett45 <gjmclaughlin19@gwmail.gwu.edu>
2018-10-25 00:52:07 -0400

    Universal template and home page

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/index')
+
+def index():
+    user = {'username': 'Garrett'}
+    return render_template('index.html', title='Home', user=user)
+
+@app.route('/forms')
+def forms():
+    return render_template('forms.html', title='Forms')
+
+@app.route('/query')
+def query():
+    return render_template('query.html', title='Query')
+
 
 if __name__ == '__main__':
     app.run()
