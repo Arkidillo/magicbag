e7668d6d03c2118be3feab18eac1704850409bd9
wbrm77 <wbrm77@gmail.com>
2018-10-25 00:43:38 -0400

    add form which also gets location and date

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, request
+from forms import DataCollect
+import datetime
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = DataCollect()
+    if form.validate_on_submit():
+        lat = request.form['lat']
+        long = request.form['long']
+        flash('Info submitted for {} with income {} at time {} and {}, {}'.format(form.name.data, form.income.data, datetime.datetime.today().strftime('%Y-%m-%d'), lat, long))
+        return redirect('/form')
+    return render_template('form.html', title='Info Form', form=form)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
