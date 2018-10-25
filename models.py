484a0e5fb74af1c1ea651ac48d04a4875df0771d
Devin Kopp <devinkopp@gwu.edu>
2018-10-25 02:57:00 -0400

    Add name cols

--- models.py
+++ models.py
@@ -3,11 +3,12 @@ from app import db
 class Form(db.Model):
     __tablename__ = 'form'
 
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String())
+    key = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
     income = db.Column(db.Float)
     last_visit_date = db.Column(db.Date)
     lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
